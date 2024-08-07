5279b8050495f29145ad9eb9741888f7e3600276
Hammad Ahmed <hammadak350@gmail.com>
2024-08-07 17:52:42 +0500

    Initialize repository, initial commit.

---
+++ android/app/src/main/kotlin/com/example/cinehub_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cinehub_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'views/home_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CineHub - Movie App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: const Color.fromARGB(255, 14, 20, 72),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
